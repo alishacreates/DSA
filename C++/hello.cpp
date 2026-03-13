4d590a3df2a2e96000528a2ab4d80b6887a8d1c4
Alisha <alishaofficial.xvi@gmail.com>
2026-03-14 00:20:52 +0530

    practicing arrays

---
+++ C++/hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//Find min Element
+int main(){
+    int n;
+    cout<< "enter n: ";
+    cin>>n;
+    int arr[n];
+
+    
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+
+   
+    return 0;
+}
