f995166a344b16beaf623f14d1461716cfc10d93
Alisha <alishaofficial.xvi@gmail.com>
2026-03-14 10:38:06 +0530

    Solved Array Problem 1

--- C++/arrays/arr.cpp
+++ C++/arrays/arr.cpp
@@ -12,15 +12,9 @@ int main(){
     for(int i = 0; i<n; i++){
         cin>>arr[i];
     }
-    int min = arr[0];
-
-    for(int i=1; i<n; i++){
-    if(min>arr[i]){
-        min = arr[i];
-    }
-}
-    cout<<"minimum is: "<< min<<endl;
-
+   for(int i=n; i>=0; i++){
+    cout<<arr[i]<<endl;
+   }
 
    
     return 0;
