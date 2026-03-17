7e53391b123f1e2d23d441d6a02c0ff02e380d29
Alisha <alishaofficial.xvi@gmail.com>
2026-03-17 05:39:44 +0530

    Solved Array Problem 2

--- C++/arrays/arr.cpp
+++ C++/arrays/arr.cpp
@@ -8,14 +8,14 @@ int main(){
     cin>>n;
     int arr[n];
 
-    
+    cout<<"enter elements"<<endl;
     for(int i = 0; i<n; i++){
         cin>>arr[i];
     }
-   for(int i=n; i>=0; i++){
+    cout<<"array in reverse: "<<endl;
+
+    for(int i=n-1; i>=0; i--){
     cout<<arr[i]<<endl;
    }
-
-   
     return 0;
 }
