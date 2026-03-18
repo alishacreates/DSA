a81650a08524010514b94928e2c005dae22f81e2
Alisha <alishaofficial.xvi@gmail.com>
2026-03-18 18:38:08 +0530

    started stl

--- C++/arrays/arr.cpp
+++ C++/arrays/arr.cpp
@@ -1,21 +1,9 @@
 #include<iostream>
 using namespace std;
 
-//Find min Element
+//count even numbers in array
 int main(){
-    int n;
-    cout<< "enter n: ";
-    cin>>n;
-    int arr[n];
+   vector <int> v; //dynamic array= size can change
 
-    cout<<"enter elements"<<endl;
-    for(int i = 0; i<n; i++){
-        cin>>arr[i];
-    }
-    cout<<"array in reverse: "<<endl;
-
-    for(int i=n-1; i>=0; i--){
-    cout<<arr[i]<<endl;
-   }
     return 0;
 }
