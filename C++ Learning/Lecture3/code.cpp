71e95729b523fae0fe5d2e7a452382ac34557f1b
Alisha <alishaofficial.xvi@gmail.com>
2026-03-18 18:37:05 +0530

    started stl

--- C++ Learning/Lecture3/code.cpp
+++ C++ Learning/Lecture3/code.cpp
@@ -3,8 +3,17 @@ using namespace std;
 
 int main(){
     //sum of odd numbers from 1 to n
+    int sum = 0;
+    int n;
     cout<<"enter n:"<<endl;
-    
-
+    cin>>n;
+    for(int i= 1; i<=n; i++){
+        if(i%2!=0){
+        sum = sum+i;
+        } else {
+            continue;
+        }
+    }
+    cout<<"sum is:"<< sum <<endl;
     return 0;
 }
