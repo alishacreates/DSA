503dbef81112d90a1ed303786a054d4eacdd6072
Alisha <alishaofficial.xvi@gmail.com>
2026-03-30 21:09:57 +0530

    done

--- C++ Learning/Lecture4/code.cpp
+++ C++ Learning/Lecture4/code.cpp
@@ -6,11 +6,16 @@ int main(){
     cout<<"enter n: ";
     cin>>n;
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            cout<<"* ";
+
+    for(int i=0; i<=n-1; i++){
+        
+        for(int j=0; j<=i; j++){
+          cout<<"*";
         }
+
         cout<<endl;
     }
+
+
     return 0;
 }
