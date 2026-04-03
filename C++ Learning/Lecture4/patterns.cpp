f8f13ae2f8085952a654c0fa7b71aeb6c50dcd3e
Alisha <alishaofficial.xvi@gmail.com>
2026-04-03 19:35:33 +0530

    pattern 5 done

--- C++ Learning/Lecture4/patterns.cpp
+++ C++ Learning/Lecture4/patterns.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 using namespace std;
-
-
 /*
 for n = 4;
-  1
-  22
-  333
-  4444*/
+  A
+  bb
+  ccc
+  dddd*/
 int main(){
     int n;
     cout<<"enter n: ";
     cin>>n;
     char character = 'A';
     for (int i = 0; i<=n; i++ ){
+        
         for(int j = 0; j<i+1; j++){
-            cout<<i+1 ;
+            cout<<character ;
         }
+        character = character+1;
         cout<<endl;
     }
     return 0;
