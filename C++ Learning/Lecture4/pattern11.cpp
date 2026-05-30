81df0924592ac345e53b6aab1d5cffbf6465e568
Alisha <alishaofficial.xvi@gmail.com>
2026-05-30 23:05:05 +0400

    completed butterfly pattern

--- C++ Learning/Lecture4/pattern11.cpp
+++ C++ Learning/Lecture4/pattern11.cpp
@@ -14,6 +14,39 @@ int main(){
     *             *
     
     */
-   
+    int n=4;
+    for(int i=0; i<=n-1; i++){
+        for(int j=0; j<=i; j++){
+        cout<<"*";
+        }
+        for(int k=n-1; k>=i+1; k--){
+            cout<<"-";
+        }
+        for(int k=n-1; k>=i+1; k--){
+            cout<<"-";
+        }
+        for(int j=0; j<=i; j++){
+        cout<<"*";
+        }
+        cout<<endl;     
+    }
+
+    // lower half
+    for(int i=0; i<=n-1; i++){
+        for(int j=n; j>=i+1; j--){
+            cout<<"*";
+        }
+        for(int k=0; k<=i-1; k++){
+            cout<<"-";
+        }
+        for(int k=0; k<=i-1; k++){
+            cout<<"-";
+        }
+        for(int j=n; j>=i+1; j--){
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
     return 0;
 }
