1103addefa43b3c2c16df8debedef272e542d757
Alisha <alishaofficial.xvi@gmail.com>
2026-05-30 13:26:02 +0400

    started pattern 11 the butterfly pattern

--- C++ Learning/Lecture4/pattern10.cpp
+++ C++ Learning/Lecture4/pattern10.cpp
@@ -3,14 +3,20 @@ using namespace std;
 int main(){
     // n=4
     int n=4;
-    for(int i=0; i<=n-1; i++){
-        for(int j=i; j>0; j--){
-            cout<<"_";
-        }
-        cout<<"*";
-        
-        cout<<endl;
 
-    }
+        for(int i = 0; i<=n-1; i++){
+             for(int j=i; j<n-1; j++){
+                cout<<"_ ";
+              }
+            for(int k=0; k<=n-1; k++){
+              cout<<"* ";
+              k=n;
+            }
+             for(int j=0; j<i; j++){
+                cout<<"_ ";
+              }
+            
+            cout<<endl;
+        }
     return 0;
 }
