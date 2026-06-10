f50c480073385bc0dadd77d512ea1eacf8b8b8d6
Alisha <alishaofficial.xvi@gmail.com>
2026-06-10 16:01:05 +0400

    pass by value

---
+++ C++ Learning/Lecture5/function3.cpp
@@ -0,0 +1,16 @@
+// pass by value
+
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b){
+    a = a + 10;
+    b = b + 10;
+    return a+b;
+}
+int main(){
+    int a = 5, b = 4;
+    sum(a,b);
+    
+    return 0;
+}
