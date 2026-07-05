f1826050a1c5729de5b1a56b04bee2bde2ab2cda
Alisha <alishaofficial.xvi@gmail.com>
2026-07-06 03:53:10 +0400

    linear search completed

---
+++ Arrays/linearSearch.cpp
@@ -0,0 +1,21 @@
+// linear search algorithm
+#include<iostream>
+using namespace std;
+
+int LinearSearch(int arr[], int size, int target){
+    for(int i= 0; i<size; i++){
+        if(arr[i] == target){
+         cout<<"the element is at index: "<<i;
+         return i;
+    }
+   }
+   return -1;
+}
+
+int main(){
+   int arr[] = {4, 2, 7, 8, 1, 2, 5};
+   int size = 7;
+   int target = 8;
+   LinearSearch(arr, size, target);
+   return 0;
+}
