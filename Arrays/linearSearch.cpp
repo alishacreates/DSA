73a79057d186731e7dbacd11d8485492d215ea8e
Alisha <alishaofficial.xvi@gmail.com>
2026-07-08 03:44:30 +0400

    started reverseArray question

--- Arrays/linearSearch.cpp
+++ Arrays/linearSearch.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int LinearSearch(int arr[], int size, int target){
     for(int i= 0; i<size; i++){
         if(arr[i] == target){
-         cout<<"the element is at index: "<<i;
          return i;
     }
    }
@@ -16,6 +15,6 @@ int main(){
    int arr[] = {4, 2, 7, 8, 1, 2, 5};
    int size = 7;
    int target = 8;
-   LinearSearch(arr, size, target);
+   cout<<"the element is at index: "<<LinearSearch(arr, size, target)<<endl;
    return 0;
 }
---
+++ Arrays/reverseArray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void ReverseArray(int arr[], int end){
+    
+}
+
+int main(){
+    int arr[7] = {4, 2, 8, 1, 2, 5};
+    int start = 0;
+
+}
