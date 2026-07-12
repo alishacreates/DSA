1eed9dfee7361a3eabd71903cd717a7cabf7a192
Alisha <alishaofficial.xvi@gmail.com>
2026-07-12 15:26:14 +0400

    array part 1 hw-1 done

--- Arrays/homeworkproblem/1.cpp
+++ Arrays/homeworkproblem/1.cpp
@@ -3,11 +3,21 @@
 #include <iostream>
 using namespace std;
 
-int SumandProduct(int arr[], int size ){
-    for(int i )
-
+void SumandProduct(int arr[], int size ){
+    int sum =0;
+    int prod= 1;
+    for(int i =0; i<size; i++){
+       sum = sum +arr[i] ;
+       prod = prod*arr[i];
+    }
+    cout<<" sum is: "<<sum<<endl;
+    cout<<" product is: "<<prod<<endl;
 }
 int main(){
-    int arr[4];
+    int arr[4] = {1, 2, 3, 4};
+    int size = 4;
+    cout<<"The sum and product of the given array is"<<endl;
+    SumandProduct(arr, size);
+
     return 0;
 }
