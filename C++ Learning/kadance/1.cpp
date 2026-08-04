a5f1d21807fe3c75ddb83dfb0ec4dee55282f25a
Alisha <alishaofficial.xvi@gmail.com>
2026-08-04 16:37:43 +0530

    Auto commit: 2026-08-04 16:37:43

--- C++ Learning/kadance/1.cpp
+++ C++ Learning/kadance/1.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
 
 int main(){
     vector<int> v = {1,2,3,4,5};
-    
+    int n = v.size();
+    cout<<"the subarrays are:"<<endl;
+    for(int st=0; st<n; st++ ){
+        for(int end =st; end<=n-1; end++){
+            cout<<"{"<<v[st]<<","<<v[end]<<"}"<<endl;
+        }
+        cout<<endl;
+    }
     return 0;
 }
