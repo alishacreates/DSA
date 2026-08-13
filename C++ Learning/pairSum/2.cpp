6a7e57071f23bb11f2a589639b859008846ca0e4
Your Name <your-email@example.com>
2026-08-13 10:00:06 +0530

    Auto commit: 2026-08-13 10:00:06

--- C++ Learning/pairSum/2.cpp
+++ C++ Learning/pairSum/2.cpp
@@ -6,19 +6,24 @@ int main(){
     vector<int> v = {2, 7, 11, 15};
     vector<int> r;
     int target = 9;
+    int size = v.size();
     int i = 0; 
-    int j = v.size() -1;
+    int j = size - 1;
     while(i<j){
         int sum = v[i] + v[j];
         if(sum == target){
-            r.push_back(i);
-            r.push_back(j);
-            cout<<r[i]<<r[j];
+            r.push_back(v[i]);
+            r.push_back(v[j]);
+            break;
         } else if(sum > target ){
             j--;
         } else {
             i++;
         }
+    }
+
+    for(int i= 0; i<r.size(); i++){
+        cout<<r[i]<<endl;
     }
     return 0;
 }
