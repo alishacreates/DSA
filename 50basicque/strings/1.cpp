8ba0f8d29163af44fcab3353c23c8b9bfaf9ae7e
alishacreates <alishaofficial.xvi@gmail.com>
2026-08-26 14:34:44 +0530

    Auto commit: 2026-08-26 14:34:44

---
+++ 50basicque/strings/1.cpp
@@ -0,0 +1,21 @@
+// reverse a sting 
+// Input:  "hello"
+// Output: "olleh"
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name;
+    string reversed;
+    cout<<"enter a string"<<endl;
+    getline(cin, name);
+    for(int i = name.length() - 1; i >= 0 ; i++){
+        reversed.push_back(name[i]);  
+    }
+    cout<<"original string: "<< name;
+    cout<<"reverse string: "<< reversed;
+    return 0;
+}
+
