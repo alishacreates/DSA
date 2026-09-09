c0680471f039feddedc8d179807c390262e4f389
alishacreates <alishaofficial.xvi@gmail.com>
2026-09-09 12:55:51 +0530

    Auto commit: 2026-09-09 12:55:51

--- Arrays/linearSearch.cpp
+++ Arrays/linearSearch.cpp
@@ -1,20 +1,16 @@
 // linear search algorithm
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int LinearSearch(int arr[], int size, int target){
-    for(int i= 0; i<size; i++){
-        if(arr[i] == target){
-         return i;
-    }
-   }
-   return -1;
-}
 
 int main(){
-   int arr[] = {4, 2, 7, 8, 1, 2, 5};
-   int size = 7;
-   int target = 8;
-   cout<<"the element is at index: "<<LinearSearch(arr, size, target)<<endl;
+  vector<int> arr = {2, 4, 5, 1};
+   int target = 5;
+   for(int i=0; i<arr.size(); i++){
+      if(arr[i] == target){
+        cout<<i<<endl;
+      }
+   }
    return 0;
 }
