f04fd74bb4b73de3f55dffb335691b9dca2ca8d0
alishacreates <alishaofficial.xvi@gmail.com>
2026-09-10 17:27:30 +0530

    Auto commit: 2026-09-10 17:27:30

---
+++ geeksforgeeks/bubb_op.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+vector <int>arr = {3,6,1,9,2};
+       bool isSwap = false;
+        int n = arr.size();
+        for(int i = 0; i<n-1; i++){
+            for(int j = 0; j<n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr[j], arr[j+1]);
+                }
+            }
+        }
+        if (!isSwap){
+            return ;
+        }
+    return 0;
+}
